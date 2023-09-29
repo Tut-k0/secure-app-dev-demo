3a972b1da966f028c53fe278ce1011fd0955ffe2
Tut-k0 <Tut_k0@protonmail.com>
2023-09-29 14:02:05 -0500

    Add test route to confirm SQL conn.

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from pyodbc import Cursor
+
+from app.database import get_db
 
 app = FastAPI()
 
@@ -11,3 +14,10 @@ async def root():
 @app.get("/{name}")
 async def say_hi(name):
     return {"message": f"Hello {name}!"}
+
+
+@app.get("/sqltest/")
+async def sql_test(db: Cursor = Depends(get_db)):
+    res = db.execute("SELECT @@VERSION;").fetchone()[0]
+    return {"sql_version": res}
+
