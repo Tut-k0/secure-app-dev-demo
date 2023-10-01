293f63db8936f9ac3ddba92b0cdf2fc3f3a978ce
Bob <tut_g99@protonmail.com>
2023-10-01 13:46:07 -0700

    Modify/Add new schemas.

--- app/schemas.py
+++ app/schemas.py
@@ -13,6 +13,7 @@ class Listing(BaseModel):
     description: str
     price: float
     seller_id: int
+    pictures: list[str] = []
 
 
 class ListingUpdate(BaseModel):
@@ -29,6 +30,13 @@ class UserCreate(BaseModel):
 
 class UserIdentifier(BaseModel):
     user_id: int
+
+
+class UserOut(BaseModel):
+    user_id: int
+    email: EmailStr
+    username: str
+    profile_picture_url: str = None
 
 
 class Token(BaseModel):
