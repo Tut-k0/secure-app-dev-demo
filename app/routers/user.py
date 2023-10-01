c067558e75ec07799f019cc14949a89a51b70db8
Bob <tut_g99@protonmail.com>
2023-09-30 21:26:18 -0700

    Formatting.

--- app/routers/user.py
+++ app/routers/user.py
@@ -5,26 +5,39 @@ from app.database import get_db
 from app.schemas import UserCreate, UserData
 from app.jwt import get_current_user
 
-router = APIRouter(prefix='/users', tags=['Users'])
+router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/", status_code=201)
 def create_user(user: UserCreate, db: Cursor = Depends(get_db)):
-    existing_user = db.execute("SELECT * FROM users WHERE username = ? OR email = ?", (user.username, user.email)).fetchone()
+    existing_user = db.execute(
+        "SELECT * FROM users WHERE username = ? OR email = ?", (user.username, user.email)
+    ).fetchone()
     if existing_user and existing_user.username == user.username:
-        raise HTTPException(status_code=409, detail=f"A user already has the username {existing_user.username}!")
+        raise HTTPException(
+            status_code=409, detail=f"A user already has the username {existing_user.username}!"
+        )
     if existing_user and existing_user.email == user.email:
-        raise HTTPException(status_code=409, detail=f"A user already has the email {existing_user.email}!")
+        raise HTTPException(
+            status_code=409, detail=f"A user already has the email {existing_user.email}!"
+        )
 
-    db.execute("INSERT INTO users (username, email, password) VALUES (?, ?, ?)", (user.username, user.email, user.password))
+    db.execute(
+        "INSERT INTO users (username, email, password) VALUES (?, ?, ?)",
+        (user.username, user.email, user.password),
+    )
     db.commit()
 
     return Response(status_code=201)
 
 
 @router.get("/{user_id}")
-def get_user(user_id: int, db: Cursor = Depends(get_db), current_user: UserData = Depends(get_current_user)):
-    user = db.execute("SELECT user_id, username, email FROM users WHERE user_id = ?", user_id).fetchone()
+def get_user(
+    user_id: int, db: Cursor = Depends(get_db), current_user: UserData = Depends(get_current_user)
+):
+    user = db.execute(
+        "SELECT user_id, username, email FROM users WHERE user_id = ?", user_id
+    ).fetchone()
     if not user:
         raise HTTPException(status_code=404, detail="User does not exist!")
 
