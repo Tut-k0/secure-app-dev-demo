9f2fbbb53976a050e6b06f3e4bde212770912f1d
Bob <tut_g99@protonmail.com>
2023-10-01 09:19:52 -0700

    General login error message fix.

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -14,10 +14,10 @@ router = APIRouter(prefix="/login", tags=["Authentication"])
 def login(credentials: OAuth2PasswordRequestForm = Depends(), db: Cursor = Depends(get_db)):
     user = db.execute("SELECT * FROM users WHERE username = ?", credentials.username).fetchone()
     if not user:
-        raise HTTPException(status_code=403, detail="User does not exist!")
+        raise HTTPException(status_code=403, detail="Invalid credentials")
 
     if not verify_password(credentials.password, user.password):
-        raise HTTPException(status_code=403, detail="Password is incorrect!")
+        raise HTTPException(status_code=403, detail="Invalid credentials")
 
     token = create_access_token(
         data={
