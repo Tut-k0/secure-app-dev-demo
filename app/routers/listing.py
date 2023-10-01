23e183df4991099858299939477f178a1e3baf07
Bob <tut_g99@protonmail.com>
2023-09-30 19:03:11 -0700

    Fix SQLi issues.

--- app/routers/listing.py
+++ app/routers/listing.py
@@ -10,17 +10,25 @@ router = APIRouter(prefix="/listings", tags=["Listings"])
 @router.post("/", response_model=Listing)
 async def create_listing(listing_data: ListingCreate, db: Cursor = Depends(get_db)):
     # Insert new listing.
-    db.execute(
-        """
+    insert_query = """
         INSERT INTO listings (title, description, price, seller_id)
-        values ('%s', '%s', %f, %d);
+        values (?, ?, ?, ?);
         """
-        % (listing_data.title, listing_data.description, listing_data.price, listing_data.seller_id)
+    db.execute(
+        insert_query,
+        listing_data.title,
+        listing_data.description,
+        listing_data.price,
+        listing_data.seller_id,
     )
-    db.commit()
-    # Return the new listing id.
-    db.execute("SELECT SCOPE_IDENTITY() AS listing_id;")
-    listing_id = db.fetchone().listing_id
+    # Retrieve the highest ascending ID matching by title or description.
+    select_query = """
+                SELECT MAX(listing_id)
+                FROM listings
+                WHERE title = ? OR description = ?;
+                """
+    db.execute(select_query, listing_data.title, listing_data.description)
+    listing_id = db.fetchone()[0]
     # Preparing output object.
     listing = vars(listing_data)
     listing["listing_id"] = listing_id
@@ -30,7 +38,7 @@ async def create_listing(listing_data: ListingCreate, db: Cursor = Depends(get_d
 
 @router.get("/{listing_id}", response_model=Listing)
 async def get_listing(listing_id: int, db: Cursor = Depends(get_db)):
-    l = db.execute("SELECT * FROM listings WHERE listing_id = %d;" % listing_id).fetchone()
+    l = db.execute("SELECT * FROM listings WHERE listing_id = ?;", listing_id).fetchone()
     if not l:
         raise HTTPException(status_code=404, detail="Listing not found")
     return Listing(
@@ -45,11 +53,16 @@ async def get_listing(listing_id: int, db: Cursor = Depends(get_db)):
 @router.get("/", response_model=list[Listing])
 async def get_listings(keyword: str | None = None, db: Cursor = Depends(get_db)):
     if keyword:
-        query = f"""
+        # Use a parameterized query to avoid SQL injection
+        query = """
                 SELECT * FROM listings
-                WHERE title LIKE '%{keyword}%' OR description LIKE '%{keyword}%'
+                WHERE title LIKE ? OR description LIKE ?
                 ORDER BY listing_id;
-                """
+            """
+        # Prepare the search pattern (including '%' wildcard) for the parameter
+        keyword_pattern = f"%{keyword}%"
+
+        db.execute(query, (keyword_pattern, keyword_pattern))
     else:
         query = "SELECT * FROM listings ORDER BY listing_id;"
 
@@ -66,17 +79,23 @@ async def update_listing(
     listing_data: ListingUpdate,
     db: Cursor = Depends(get_db),
 ):
-    existing_listing = db.execute("SELECT * FROM listings WHERE listing_id = %s;" % listing_id).fetchone()
+    existing_listing = db.execute("SELECT * FROM listings WHERE listing_id = ?;", listing_id).fetchone()
     if not existing_listing:
         raise HTTPException(status_code=404, detail="Listing not found")
 
     # Update the listing data
     update_query = """
         UPDATE listings
-        SET title = '%s', description = '%s', price = %f
-        WHERE listing_id = %d;
-        """ % (listing_data.title, listing_data.description, listing_data.price, listing_id)
-    db.execute(update_query)
+        SET title = ?, description = ?, price = ?
+        WHERE listing_id = ?;
+        """
+    db.execute(
+        update_query,
+        listing_data.title,
+        listing_data.description,
+        listing_data.price,
+        listing_id,
+    )
     db.commit()
 
     # Return the updated listing
@@ -96,12 +115,12 @@ async def delete_listing(
     db: Cursor = Depends(get_db),
 ):
     # Check if the listing exists
-    existing_listing = db.execute("SELECT * FROM listings WHERE listing_id = %s;" % listing_id).fetchone()
+    existing_listing = db.execute("SELECT * FROM listings WHERE listing_id = ?;", listing_id).fetchone()
     if not existing_listing:
         raise HTTPException(status_code=404, detail="Listing not found")
 
     # Delete the listing
-    db.execute("DELETE FROM listings WHERE listing_id = %s;" % listing_id)
+    db.execute("DELETE FROM listings WHERE listing_id = ?;", listing_id)
     db.commit()
 
     return Response(status_code=204)
